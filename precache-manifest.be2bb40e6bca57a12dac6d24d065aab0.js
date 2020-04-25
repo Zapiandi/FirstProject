bd94bd14389cb37f0d27aeda408371cd0a67ee13
Zapiandi <63498179+Zapiandi@users.noreply.github.com>
2020-04-25 12:15:39 +0200

    Updates

---
+++ precache-manifest.be2bb40e6bca57a12dac6d24d065aab0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a62c11d7b4abe94e6dc9b2ab6423522d",
+    "url": "/FirstProject/index.html"
+  },
+  {
+    "revision": "a76249b7330f2fa9434c",
+    "url": "/FirstProject/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "f554328a3898f3773395",
+    "url": "/FirstProject/static/css/main.f45f4b8f.chunk.css"
+  },
+  {
+    "revision": "a76249b7330f2fa9434c",
+    "url": "/FirstProject/static/js/2.2f7a48c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FirstProject/static/js/2.2f7a48c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f554328a3898f3773395",
+    "url": "/FirstProject/static/js/main.4cba9ab4.chunk.js"
+  },
+  {
+    "revision": "cfd274f6aaebd18a4718",
+    "url": "/FirstProject/static/js/runtime-main.1fefaf9d.js"
+  },
+  {
+    "revision": "5ce3fc11b995237a76e13dba89a9b3ea",
+    "url": "/FirstProject/static/media/Robot World.5ce3fc11.woff"
+  }
+]);
